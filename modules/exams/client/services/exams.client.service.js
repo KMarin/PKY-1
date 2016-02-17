9a48e86454c5c63958dc0690aef1ac9c92c6fe71
Brandon <bduong30@ufl.edu>
2016-02-17 18:29:45 -0500

    fixed all the csslint syntax whitespace errors

--- modules/exams/client/services/exams.client.service.js
+++ modules/exams/client/services/exams.client.service.js
@@ -5,52 +5,52 @@
     .module('exams.services')
     .factory('ExamsService', ['$http', function($http){
 		
-		var exam_url_base = "/api/exams";
-		var question_url_base = "/api/questions";
+      var exam_url_base = '/api/exams';
+      var question_url_base = '/api/questions';
 		
 		// exams
-		return {
-			get_exams : function(){
-				return $http.get(exam_url_base);
-			},
+      return {
+        get_exams : function(){
+          return $http.get(exam_url_base);
+        },
 			
-			get_exam : function(id){
-				return $http.get(exam_url_base + "/" + id);
-			},	
+        get_exam : function(id){
+          return $http.get(exam_url_base + '/' + id);
+        },	
 			
-			update_exam : function(exam){
-				return $http.put(exam_url_base + "/" + exam.id, exam);
-			},
+        update_exam : function(exam){
+          return $http.put(exam_url_base + '/' + exam.id, exam);
+        },
 			
-			create_exam : function(exam){
-				return $http.post(exam_url_base, exam);
-			},
+        create_exam : function(exam){
+          return $http.post(exam_url_base, exam);
+        },
 			
-			delete_exam : function(id){
-				return $http.delete(exam_url_base + "/" + id);
-			},
+        delete_exam : function(id){
+          return $http.delete(exam_url_base + '/' + id);
+        },
 			
 			// questions
-			get_questions : function(){
-				return $http.get(question_url_base);
-			},
+        get_questions : function(){
+          return $http.get(question_url_base);
+        },
 			
-			get_question : function(id){
-				return $http.get(question_url_base + "/" + id);
-			},	
+        get_question : function(id){
+          return $http.get(question_url_base + '/' + id);
+        },	
 			
-			update_question : function(question){
-				return $http.put(question_url_base + "/" + question.id, question);
-			},
+        update_question : function(question){
+          return $http.put(question_url_base + '/' + question.id, question);
+        },
 			
-			create_question : function(question){
-				return $http.post(question_url_base, question);
-			},
+        create_question : function(question){
+          return $http.post(question_url_base, question);
+        },
 			
-			delete_question : function(id){
-				return $http.delete(question_url_base + "/" + id);
-			}
-		};
+        delete_question : function(id){
+          return $http.delete(question_url_base + '/' + id);
+        }
+      };
 
-	}]);
+    }]);
 })();
